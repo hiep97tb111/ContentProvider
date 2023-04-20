f4c9923e0944aa7c4dea49a31477ee45958df6ab
HiepPD <hieppdutck59@gmail.com>
2023-04-21 00:45:21 +0700

    first commit

---
+++ app/src/main/java/com/example/contentproviderdemo/MainAct.kt
@@ -0,0 +1,13 @@
+package com.example.contentproviderdemo
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainAct : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.act_main)
+
+    }
+}
